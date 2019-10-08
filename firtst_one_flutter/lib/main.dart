1865d6a78a25774a7362916d0525af775ddc9e34
ZainRasheed <rasheedulla97@gmail.com>
2019-10-08 20:56:34 +0530

    Buttons with Null functions

--- firtst_one_flutter/lib/main.dart
+++ firtst_one_flutter/lib/main.dart
@@ -6,17 +6,51 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(FirstApp());
 
-class FirstApp extends StatelessWidget{
-  @override
-  Widget build(  context) {
-    // TODO: implement build
-//    return MaterialApp(home: Text('Hello!'),);
-    return MaterialApp(home: Scaffold(
-      appBar: AppBar(
-        title: Text("My first app"),
-      ),
-      body: Text("This the body tetx"),
-    ),);
+class FirstApp extends StatelessWidget {
+
+  void answerToQuestion1(){
+    print("You are of type 1");
+  }
+  void answerToQuestion2(){
+    print("You are of type 2");
+  }
+  void answerToQuestion3(){
+    print("You are of type 3");
   }
 
+  @override
+  Widget build(BuildContext context) {
+//    return MaterialApp(home: Text('Hello!'),);
+
+    var questions = [
+      "What is your fav colour?",
+      "What is your Fav season",
+      "What is your fav movie?"
+    ];
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("My first app"),
+        ),
+        body: Column(
+          children: <Widget>[
+            Text(questions[0]),
+            RaisedButton(
+              child: Text("Answer 1"),
+              onPressed: answerToQuestion1,
+            ),
+            RaisedButton(
+              child: Text("Answer 2"),
+              onPressed: answerToQuestion2,
+            ),
+            RaisedButton(
+              child: Text("Answer 3"),
+              onPressed: answerToQuestion3,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
